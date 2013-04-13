4a20fe0e8c30e92d7f69f98236608234db115105
joey moreland <joey.moreland@gmail.com>
2013-04-13 10:17:26 -0400

    Initial repo

---
+++ pcb/airswimmer-controller-30.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+"povray" +W1600 +H1200 +A0.3 +FN +L"/opt/local/bin/povray" +L"/Users/jmoreland/Applications/eagle3d/ulp//../povray" +O"/Users/jmoreland/.dropbox-alt/Dropbox/Projects/airswimmer/pcb/airswimmer-controller-30.png" /Users/jmoreland/.dropbox-alt/Dropbox/Projects/airswimmer/pcb/airswimmer-controller-30.pov 
