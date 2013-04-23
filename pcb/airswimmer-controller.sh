174661cb11d1dd27b94a42a5992f07fefaa2c972
joey moreland <joey.moreland@gmail.com>
2013-04-22 22:56:09 -0400

    Reorg to the standard JCM layout.

--- pcb/airswimmer-controller.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-"povray" +W1600 +H1200 +A0.3 +FN +L"/opt/local/bin/povray" +L"/Users/jmoreland/Applications/eagle3d/ulp//../povray" +O"/Users/jmoreland/.dropbox-alt/Dropbox/Projects/airswimmer/pcb/airswimmer-controller.png" /Users/jmoreland/.dropbox-alt/Dropbox/Projects/airswimmer/pcb/airswimmer-controller.pov 
